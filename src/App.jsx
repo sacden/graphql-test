727f6149e3bfdce87c5aa49847eac28d6cf0e9d6
sacden <sacden2009@gmail.com>
2023-12-09 18:03:20 +0100

    task2: add functionality for hiding columns

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,39 @@ import { ALL_PRODUCTS } from "./apollo/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [columns, setColumns] = useState([
+    { id: 1, label: "Name", state: true },
+    { id: 2, label: "Items count", state: true },
+    { id: 3, label: "Archived", state: true },
+    { id: 4, label: "Icon", state: true },
+    { id: 5, label: "Created", state: true },
+    { id: 6, label: "Last import", state: true },
+  ]);
   const { loading, error, data } = useQuery(ALL_PRODUCTS);
+
+  const renderCell = (column, product) => {
+    switch (column.id) {
+      case 1:
+        return product.name;
+      case 2:
+        return product.itemsCount;
+      case 3:
+        return product.archived ? "true" : "false";
+      case 4:
+        return product.icon;
+      case 5:
+        return product.createdAt;
+      case 6:
+        return product.lastImport;
+      default:
+        return null;
+    }
+  };
+
+  const onSelectCheckbox = (e) => {
+    const updatedColumns = columns.map((column) => (column.id === parseInt(e.target.value) ? { ...column, state: !column.state } : column));
+    setColumns(updatedColumns);
+  };
 
   useEffect(() => {
     if (data && data.collection) {
@@ -23,30 +55,34 @@ function App() {
   return (
     <>
       {/* {JSON.stringify(data)} */}
+
+      {columns.map((column) => {
+        return (
+          <div className="form-check" key={column.id}>
+            <input className="form-check-input" type="checkbox" value={column.id} id={`${column.id}`} checked={column.state === true} onChange={(e) => onSelectCheckbox(e)} />
+            <label className="form-check-label" htmlFor={`${column.id}`}>
+              {column.label}
+            </label>
+          </div>
+        );
+      })}
+
       <table className="table table-striped table-hover">
         <thead>
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Items count</th>
-            <th scope="col">Archived</th>
-            <th scope="col">Icon</th>
-            <th scope="col">Created</th>
-            <th scope="col">Last import</th>
+            {columns.map((column) => {
+              return column.state ? (
+                <th key={column.id} scope="col">
+                  {column.label}
+                </th>
+              ) : null;
+            })}
           </tr>
         </thead>
         <tbody>
-          {products.map((product) => {
-            return (
-              <tr key={product.id}>
-                <td>{product.name}</td>
-                <td>{product.itemsCount}</td>
-                <td>{product.archived === true ? "true" : "false"}</td>
-                <td>{product.icon}</td>
-                <td>{product.createdAt}</td>
-                <td>{product.lastImport}</td>
-              </tr>
-            );
-          })}
+          {products.map((product) => (
+            <tr key={product.id}>{columns.map((column) => column.state && <td key={column.id}>{renderCell(column, product)}</td>)}</tr>
+          ))}
         </tbody>
       </table>
     </>
